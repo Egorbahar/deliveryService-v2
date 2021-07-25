80a2ec11ea515298aed19bd374b2c2ac742fd82c
User <egorbahar@gmail.com>
2021-07-25 09:19:21 +0300

    Add configuration for Logback

--- core/starter/src/main/java/com/exposit/Starter.java
+++ core/starter/src/main/java/com/exposit/Starter.java
@@ -9,16 +9,5 @@ public class Starter {
 
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
-//        System.out.println("Enter the name of store:");
-//        Scanner scanner = new Scanner(System.in);
-//        String name = scanner.nextLine();
-//        System.out.println("Enter the address of store:");
-//        scanner = new Scanner(System.in);
-//        String address = scanner.nextLine();
-//        try {
-//            storeService.add(new Store(name, address));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 }
