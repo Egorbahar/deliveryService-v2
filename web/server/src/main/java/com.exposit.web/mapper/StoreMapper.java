6093caf1a49924b8c4a909eb968f35d1db89e904
User <egorbahar@gmail.com>
2021-08-10 17:49:00 +0300

    Add StoreMapper

---
+++ web/server/src/main/java/com.exposit.web/mapper/StoreMapper.java
@@ -0,0 +1,21 @@
+package com.exposit.web.mapper;
+
+import com.exposit.persistence.entity.Store;
+import com.exposit.web.dto.StoreDto;
+import org.mapstruct.*;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface StoreMapper {
+    Store toStore(StoreDto productDto);
+
+    StoreDto toStoreDto(Store product);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    void updateEntity(@MappingTarget Store store, StoreDto storeDto);
+
+    @IterableMapping(elementTargetType = StoreDto.class)
+    List<StoreDto> toStoreDtoList(Collection<Store> stores);
+}
